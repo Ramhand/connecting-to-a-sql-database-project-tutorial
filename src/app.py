e55205b967f6fd00f9eaf2e4043dc07f2afac6ad
Ramhand <166147778+Ramhand@users.noreply.github.com>
2024-06-18 00:25:35 +0000

    Solved

--- src/app.py
+++ src/app.py
@@ -1,15 +1,42 @@
 import os
-from sqlalchemy import create_engine
+import sqlalchemy.engine as eng
+from sqlalchemy.sql import text
 import pandas as pd
 from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+files = ['src/sql/create.sql', 'src/sql/insert.sql', 'src/sql/drop.sql']
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+def connect():
+    global engine
+    st = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+    engine = eng.create_engine(st)
+    pop_goes_the_sql()
+    lock_goes_the_sql()
+    drop_goes_the_sql()
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+def pop_goes_the_sql():
+    global engine
+    global files
+    for i in range(2):
+        with engine.connect() as con:
+            with open(files[i]) as file:
+                con.execute(text(file.read()))
+                con.close()                
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+def lock_goes_the_sql():
+    global engine
+    df = pd.read_sql('books', engine.connect())
+    print(df)
+
+def drop_goes_the_sql():
+    global engine
+    global files
+    with engine.connect() as con:
+        with open(files[-1]) as file:
+            query = text(file.read())
+            con.execute(query)
+
+connect()
--- src/sql/insert.sql
+++ src/sql/insert.sql
@@ -1,4 +1,4 @@
--- publishers
+
 INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
 INSERT INTO publishers(publisher_id, name) VALUES (2, 'A Book Apart');
 INSERT INTO publishers(publisher_id, name) VALUES (3, 'A K PETERS');
@@ -7,7 +7,7 @@ INSERT INTO publishers(publisher_id, name) VALUES (5, 'Addison Wesley');
 INSERT INTO publishers(publisher_id, name) VALUES (6, 'Albert&Sweigart');
 INSERT INTO publishers(publisher_id, name) VALUES (7, 'Alfred A. Knopf');
 
--- authors 
+
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (1, 'Merritt', null, 'Eric');
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (2, 'Linda', null, 'Mui');
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (3, 'Alecos', null, 'Papadatos');
@@ -17,7 +17,7 @@ INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (6, '
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (7, 'Yuval', 'Noah', 'Harari');
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (8, 'Paul', null, 'Albitz');
 
--- books
+
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (1, 'Lean Software Development: An Agile Toolkit', 240, 4.17, '9780320000000', '2003-05-18', 5);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (2, 'Facing the Intelligence Explosion', 91, 3.87, null, '2013-02-01', 7);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (3, 'Scala in Action', 419, 3.74, '9781940000000', '2013-04-10', 1);
@@ -29,7 +29,7 @@ INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, pu
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (9, 'The Apollo Guidance Computer: Architecture And Operation (Springer Praxis Books / Space Exploration)', 439, 4.29, '9781440000000', '2010-07-01', 6);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (10, 'Minds and Computers: An Introduction to the Philosophy of Artificial Intelligence', 222, 3.54, '9780750000000', '2007-02-13', 7);
 
--- book authors
+
 INSERT INTO book_authors (book_id, author_id) VALUES (1, 1);
 INSERT INTO book_authors (book_id, author_id) VALUES (2, 8);
 INSERT INTO book_authors (book_id, author_id) VALUES (3, 7);
